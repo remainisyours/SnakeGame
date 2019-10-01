564f466babe87c46634dd2536345cd44b6e83702
regexistaken <32675731+AnkitGupta17134@users.noreply.github.com>
2019-10-01 22:53:36 +0530

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,16 +1,5 @@
 public class Game
 {
-  int origin;
-  public Game(int a)
-  {
-    this.origin = a;
-    System.out.println("Game is Starting");
-  }
-  
-  public void Left()
-  {
-    this.origin--;
-  }
   
   public static void main(String[] args)
   {

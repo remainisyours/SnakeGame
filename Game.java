86d5edd53f2ce98f30e23c15f467128a93ac97d8
remainisyours <44507677+remainisyours@users.noreply.github.com>
2019-10-01 17:24:24 +0000

    Moving Left
    Moving Left Origin

--- Game.java
+++ Game.java
@@ -1,5 +1,16 @@
 public class Game
 {
+  int origin;
+  public Game(int a)
+  {
+    this.origin = a;
+    System.out.println("Game is Starting");
+  }
+  
+  public void Left()
+  {
+    this.origin--;
+  }
   
   public static void main(String[] args)
   {

e7db8e9469259629790f9d9f941596a70de5bedb
remainisyours <44507677+remainisyours@users.noreply.github.com>
2019-10-01 17:20:57 +0000

    Initialization
    Initialization

--- Game.java
+++ Game.java
@@ -1,5 +1,12 @@
 public class Game
 {
+  int origin;
+  public Game(int a)
+  {
+    this.origin = a;
+    System.out.println("Game is Starting");
+  }
+  
   public static void main(String[] args)
   {
     System.out.println("Hello Snake");
